74b8fcdf03e80bc03eec98eb71ce3adcf609fb61
Abrar Mahmud Hasan <abrarhasan2003@gmail.com>
2026-02-27 02:22:12 +0600

    Add divide function with error handling

--- src/main.py
+++ src/main.py
@@ -6,4 +6,4 @@ print(datetime.datetime.now())
 
 print("The sum of 5 and 3 is:", utils.add(5, 3))
 print("The difference of 5 and 3 is:", utils.subtract(5, 3))
-
+print("The product of 5 and 3 is:", utils.multiply(5, 3))
