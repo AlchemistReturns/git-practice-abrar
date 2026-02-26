623845293734f699b6491d5fc57fa36042661495
Abrar Mahmud Hasan <abrarhasan2003@gmail.com>
2026-02-27 02:14:35 +0600

    Add basic calculator fucntions

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
 import datetime
+import utils
 
 print("Hi, I am Abrar")
 print(datetime.datetime.now())
+
+print("The sum of 5 and 3 is:", utils.add(5, 3))
+print("The difference of 5 and 3 is:", utils.subtract(5, 3))
 
